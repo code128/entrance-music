36f8efddae11787744bce072ac73be6ee8152c1d
Joshua Bloom <joshbloom@gmail.com>
2017-08-29 18:34:37 -0700

    Adding an example settings file

---
+++ settings-example.py
@@ -0,0 +1,6 @@
+##--- Settings ---#
+soundEffectsDirectory = '/Users/EntranceMusic'
+songTimeOutInSeconds = "8"
+sleepTimeInSeconds = 3
+url = "http://localhost/" #Admin page URL
+payload = "login_id=USERNAME&login_pw=PASSWORD"
